c22c144fdb848b07a29439d69cfca6b9409e042d
Scott Workman <scottworkman@users.noreply.github.com>
2022-04-28 13:25:54 -0400

    update download links
    - resolves #6

--- models/download_models.sh
+++ models/download_models.sh
@@ -1,13 +1,13 @@
-#!/usr/bin/env sh
+#!/bin/bash
 #
 # This script downloads and unpacks the model files, solver definitions, 
 # and learned network weights for all networks described in the paper.
 
-if [ $1 == '--demo' ]
+if [ "$1" == '--demo' ]
 then
-  URL='http://amos.csr.uky.edu/modelzoo/deephorizon/deephorizon_single.zip'
+  URL='https://wustl.box.com/shared/static/0s5xli5uisxn4x138oxybc9l28n5whpy.zip'
 else
-  URL='http://amos.csr.uky.edu/modelzoo/deephorizon/deephorizon.zip'
+  URL='https://wustl.box.com/shared/static/kk7hcdsp82s4nv53cckxce26xhroggnu.zip'
 fi
   
 echo "Downloading..."
